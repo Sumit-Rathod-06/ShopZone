4914a95c64512e5ac61d5631fed835cc7f7750e4
Sumit-Rathod-06 <sumitrrathod06@gmail.com>
2024-10-30 14:38:03 +0530

    Linked DB to backend And Implemented Logic of Login and Register

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import bodyParser from "body-parser";
+import pg from "pg"
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -7,6 +8,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();  
 const port = 3000;
+
+const db = new pg.Client({
+  user : "postgres",
+  host : "localhost",
+  database : "shopzone",
+  password : "Sumit@06",
+  port : 5432
+});
 
 app.use(express.static("public"));
 
@@ -16,9 +25,84 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-// app.post("/submit",(req,res) => {
-//   console.log(req.body);
-// })
+db.connect();
+
+app.post("/login-customer",async (req,res)=>{
+
+  const name = req.body["username"];
+  const password = req.body["password"];
+
+  const resul = await db.query("Select password from customer where name = $1",[name]);
+
+  if(resul.rows.length > 0)
+  {
+    const user = resul.rows[0];
+    const dbpass = user.password;
+
+    if(dbpass === password)
+    {
+      res.send("<h1>Welcome to Customer HomePage</h1>");
+    }
+    else{
+      res.send("<h1>Incorrect Password</h1>");
+    }
+  }
+  else
+  {
+    res.send("<h1>Customer not Found</h1>");
+  }
+})
+
+app.post("/login-seller",async (req,res)=>{
+
+  const name = req.body["username"];
+  const password = req.body["password"];
+
+  const resul = await db.query("Select password from seller where name = $1",[name]);
+
+  if(resul.rows.length > 0)
+  {
+    const user = resul.rows[0];
+    const dbpass = user.password;
+
+    if(dbpass === password)
+    {
+      res.send("<h1>Welcome to Seller HomePage</h1>");
+    }
+    else{
+      res.send("<h1>Incorrect Password</h1>");
+    }
+  }
+  else
+  {
+    res.send("<h1>Seller not Found</h1>");
+  }
+})
+
+app.post("/register-customer",async (req,res)=>{
+
+  const name = req.body["username"];
+  const password = req.body["password"];
+  const age = req.body["age"];
+  const phone_no = req.body["phone_no"];
+
+  await db.query("Insert Into customer(name,password,age,phone_no) Values ($1,$2,$3,$4)",[name,password,age,phone_no]);
+  res.sendFile(__dirname + "/public/login-customer.html");
+})
+
+app.post("/register-seller",async (req,res)=>{
+
+  const name = req.body["username"];
+  const password = req.body["password"];
+  const age = req.body["age"];
+  const phone_no = req.body["phone_no"];
+
+  await db.query("Insert Into seller(name,password,age,phone_no) Values ($1,$2,$3,$4)",[name,password,age,phone_no]);
+  
+  db.end();
+  
+  res.sendFile(__dirname + "/public/login-seller.html");
+})
 
 app.listen(port,()=>{
     console.log(`Server Running on Port ${port}`);
