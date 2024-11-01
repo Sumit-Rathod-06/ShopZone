31ccefcb6570066a43fd8d8ea881c3e2cdb2d8fe
Sumit-Rathod-06 <sumitrrathod06@gmail.com>
2024-11-01 22:25:16 +0530

    Added Add and Delete Product feature

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const app = express();
 const port = 3000;
 
 let products = [];
+var name;
+var password;
 
 const db = new pg.Client({
   user : "postgres",
@@ -31,8 +33,8 @@ db.connect();
 
 app.post("/login-customer",async (req,res)=>{
 
-  const name = req.body["username"];
-  const password = req.body["password"];
+  name = req.body["username"];
+  password = req.body["password"];
 
   const resul = await db.query("Select password from customer where name = $1",[name]);
 
@@ -71,8 +73,8 @@ app.post("/login-customer",async (req,res)=>{
 
 app.post("/login-seller",async (req,res)=>{
 
-  const name = req.body["username"];
-  const password = req.body["password"];
+  name = req.body["username"];
+  password = req.body["password"];
 
   const resul = await db.query("Select password from seller where name = $1",[name]);
   const id = await db.query("Select id from seller where name = $1",[name]);
@@ -112,8 +114,8 @@ app.post("/login-seller",async (req,res)=>{
 
 app.post("/register-customer",async (req,res)=>{
 
-  const name = req.body["username"];
-  const password = req.body["password"];
+  name = req.body["username"];
+  password = req.body["password"];
   const age = req.body["age"];
   const phone_no = req.body["phone_no"];
 
@@ -123,16 +125,82 @@ app.post("/register-customer",async (req,res)=>{
 
 app.post("/register-seller",async (req,res)=>{
 
-  const name = req.body["username"];
-  const password = req.body["password"];
+  name = req.body["username"];
+  password = req.body["password"];
   const age = req.body["age"];
   const phone_no = req.body["phone_no"];
 
-  await db.query("Insert Into seller(name,password,age,phone_no) Values ($1,$2,$3,$4)",[name,password,age,phone_no]);
-  
-  db.end();
-  
+  await db.query("Insert Into seller(name,password,age,phone_no) Values ($1,$2,$3,$4)",[name,password,age,phone_no]);  
   res.sendFile(__dirname + "/public/login-seller.html");
+})
+
+app.get("/add-product",(req,res)=>{
+  res.sendFile(__dirname + "/public/add-product.html");
+})
+
+app.post("/add-product-redirect",async (req,res) => {
+
+  const pname = req.body["pname"];
+  const desc = req.body["description"];
+  const price = req.body["price"];
+
+  const id = await db.query("Select id from seller where name = $1",[name]);
+
+  await db.query("Insert into product(sid,pname,price,description) values($1,$2,$3,$4)",[id.rows[0].id,pname,price,desc]);
+
+  const pro = await db.query("Select * From product Where sid = $1",[id.rows[0].id]);
+  if(pro.rows <= 0)
+  {
+    products = [];
+  }
+  else
+  {
+    products = pro.rows;
+  }
+  res.render("home-seller.ejs",
+    {
+        products: products
+    }
+  )
+  
+})
+
+app.get("/deleteproduct/:id", async (req, res) => {
+  
+  await db.query("Delete from product where id = $1",[req.params.id]);
+
+  const id = await db.query("Select id from seller where name = $1",[name]);
+  const pro = await db.query("Select * From product Where sid = $1",[id.rows[0].id]);
+  if(pro.rows <= 0)
+  {
+    products = [];
+  }
+  else
+  {
+    products = pro.rows;
+  }
+  res.render("home-seller.ejs",
+    {
+        products: products
+    }
+  )});
+
+app.get("/buyproduct/:id",async (req,res) => {
+
+  const pro = await db.query("Select * From product Where sid = $1",[req.params.id]);
+  if(pro.rows <= 0)
+  {
+    products = [];
+  }
+  else
+  {
+    products = pro.rows;
+  }
+  res.render("buy-product.ejs",
+    {
+        products: products
+    }
+  )
 })
 
 app.listen(port,()=>{
